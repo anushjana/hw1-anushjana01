c462011e3cf2ba99a64b71fa32a298e4cc3f0500
anushjana <124036246+anushjana@users.noreply.github.com>
2023-02-05 15:57:44 +0400

    Add files via upload

--- linear_regression.py
+++ linear_regression.py
@@ -1,6 +1,3 @@
-"""
-Write full code for CustomLinearRegression Class
-"""
 import numpy as np
 from sklearn.datasets import make_regression
 import matplotlib.pyplot as plt
@@ -10,78 +7,49 @@ from sklearn.linear_model import Ridge
 
 from typing import Union
 
-
 class CustomLinearRegression:
     def __init__(self, C: Union[float, int] = 0, random_state: int = 42):
         self.random_state = random_state
-        self.C = C  # L2 regularization coefficient, you will need this in gradient computation
+        self.C = C
         self.W = None
         self.b = None
 
     def init_weights(self, input_size: int, output_size: int):
-        """
-        Initialize weights and biases
 
-        `W` -  matrix with shapes (input_size, output_size)
-        Initialize with random numbers from Normal distribution with mean 0 and std 0.01
-        `b` - vector with shape (1, output_size), initialize with 0s
-        """
         np.random.seed(self.random_state)
-        self.W = ...  # TODO
-        self.b = ...  # TODO
+        self.W = np.random.normal(0, 0.01, (input_size, output_size))
+        self.b = np.zeros((1, output_size))
 
     def fit(self, X: np.ndarray, y: np.ndarray, num_epochs: int = 1000, lr: float = 0.001):
-        """Train model linear regression with gradient descent
 
-        Parameters
-        ----------
-        X: with shape (num_samples, input_shape)
-        y: with shape (num_samples, output_shape)
-        num_epochs: number of interactions of gradient descent
-        lr: step of linear regression
-
-        Returns
-        -------
-        None
-        """
         m, n = X.shape
         self.init_weights(X.shape[1], y.shape[1])
 
         for _ in range(num_epochs):
             preds = self.predict(X)
-            # compute gradients without loops, only use numpy.
-            # IMPORTANT don't forget to compute gradients for L2 regularization
-            b_grad = ...  # TODO
-            W_grad = ...  # TODO
-            self.W = ...  # TODO
-            self.b = ...  # TODO
-
+            b_grad = 2 / m * np.sum(preds - y) + 2 * self.C * self.b
+            W_grad = 2 / m * X.T.dot(preds - y) + 2 * self.C * self.W
+            self.W = self.W - lr * W_grad
+            self.b = self.b - lr * b_grad
     def predict(self, X: np.ndarray) -> np.ndarray:
-        """
-        Do your predictions here :)
-        """
-        return ...  # TODO
+
+        return X.dot(self.W) + self.b
 
 
 if __name__ == "__main__":
-    # TODO run this part of the code to generate plot, save plot and upload to github
-    #  you are free to play with parameters
-    custom_l2 = CustomLinearRegression(C=10, random_state=42)
 
-    # TODO also use linear regression without L2, implemented in CustomLinearRegression.
-    custom_lin_reg = ...
+    custom_l2 = CustomLinearRegression(C=10, random_state=42)
+    custom_lin_reg = CustomLinearRegression(random_state=42)
     lg_sk = lg_sklearn()
     ridge = Ridge(alpha=10)
 
     X, y = make_regression(1000, n_features=1, n_targets=1, random_state=42, noise=0)
     y = np.expand_dims(y, 1)
 
-    # adding anomalous datapoint to see effect of regression
-    # you are free to comment this part to see effect on normal linear data
     X = np.vstack((X, np.array([X.max() + 20])))
     y = np.vstack((y, np.array([y.max() + 10])))
 
-    # fitting models
+
     custom_l2.fit(X, y)
     y_hat_l2 = custom_l2.predict(X)
 
@@ -94,7 +62,6 @@ if __name__ == "__main__":
     ridge.fit(X, y)
     y_hat_ridge = ridge.predict(X)
 
-    # plotting models
     plt.scatter(X, y)
     plt.plot(X, y_hat_l2, color="red", label="Custom L2")
     plt.plot(X, y_hat_lin, color="k", label="Custom Lin reg")
